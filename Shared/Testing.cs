56977f7b559644f1963cba5de1c5f7a4a43a7788
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2025-01-04 12:26:06 +1000

    Initial commit

---
+++ Shared/Testing.cs
@@ -0,0 +1,16 @@
+﻿namespace Shared;
+
+public static class Testing
+{
+    private static bool _initialized = false;
+
+    [Before(Assembly)]
+    public static void GlobalSetup()
+    {
+        _initialized = true;
+    }
+
+    public static bool IsInitialized => _initialized;
+
+    public static void Log() => Console.WriteLine("Starting up...");
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace Tests;
+
+public class UnitTest1
+{
+    [Before(TestDiscovery)]
+    public static void PreDiscovery()
+    {
+        Shared.Testing.Log();
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        // If the setup hook from the shared library has been successfully called, this test should pass
+        var initialized = Shared.Testing.IsInitialized;
+        await Assert.That(initialized).IsTrue();
+    }
+}
